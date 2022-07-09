2e2575142166e413e6c1c65a029d7439b773591e
OmerM <83801626+MaskeyDude@users.noreply.github.com>
2022-07-09 11:05:01 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const Hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const CG_Btn = document.querySelector(".CG_Btn");
+const Hex_Code = document.querySelector(".Hex_Code");
+
+CG_Btn?.addEventListener('click', function () {
+    let Code = '#';
+    for (let i = 0; i < 6; i++) {
+        Code += Hex[Get_Random_Number()];
+    }
+    Hex_Code.textContent = Code;
+    document.body.style.backgroundColor = Code;
+});
+
+function Get_Random_Number() {
+    return Math.floor(Math.random() * Hex.length);
+}
